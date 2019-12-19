98a80477ccc4df03c37460c80d35eac47720862d
Martin Middel <martin.middel@liones.nl>
2019-12-19 12:34:21 +0100

    Extend the parser to compile caret and dollar
    This moves the compiler more towards also support XPath-style patterns, which differ slightly from
    XSD-style patterns

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 import { compileVM } from 'whynot';
 import { compileRegExp } from './compiler';
-import { parse } from './parser';
+import { generateParser } from './parser';
 
 function toCodePoints(str: string): number[] {
 	return [...str].map(c => c.codePointAt(0)!);
@@ -8,13 +8,18 @@ function toCodePoints(str: string): number[] {
 
 export type MatchFn = (str: string) => boolean;
 
+export type Options = {
+	language: 'xsd' | 'xpath';
+};
+
 /**
  * @param pattern Pattern to compile
+ * @param options Additional options for the compiler
  *
  * @return a matcher function, or null if the pattern uses unsupported features
  */
-export function compile(pattern: string): MatchFn {
-	const ast = parse(pattern);
+export function compile(pattern: string, options: Options = { language: 'xsd' }): MatchFn {
+	let ast = generateParser(options)(pattern);
 
 	const vm = compileVM<number>(assembler => {
 		compileRegExp(assembler, ast);
@@ -22,6 +27,8 @@ export function compile(pattern: string): MatchFn {
 	});
 
 	return function match(str: string): boolean {
-		return vm.execute(toCodePoints(str)).success;
+		const codepoints =
+			options.language === 'xpath' ? [-1, ...toCodePoints(str), -2] : toCodePoints(str);
+		return vm.execute(codepoints).success;
 	};
 }
