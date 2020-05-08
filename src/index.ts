c050f24f8c8b44717f2b55944ce75228589990c9
Stef Busking <stef.busking@gmail.com>
2020-05-08 12:36:53 +0200

    Add typings and API docs

--- src/index.ts
+++ src/index.ts
@@ -7,17 +7,37 @@ function toCodePoints(str: string): number[] {
 	return [...str].map(c => c.codePointAt(0)!);
 }
 
+/**
+ * A function used to validate strings against the pattern it represents. Accepts a single string
+ * and returns a boolean indicating whether it matches the pattern.
+ *
+ * @public
+ */
 export type MatchFn = (str: string) => boolean;
 
+/**
+ * Options to control pattern compilation.
+ *
+ * Currently, only a language option is supported, defaulting to 'xsd'. Set this to 'xpath' to
+ * compile the pattern with XPath syntax and semantics. Note that not all XPath-specific features
+ * are currently supported. See https://github.com/bwrrp/xspattern.js/issues/9 for details.
+ *
+ * @public
+ */
 export type Options = {
 	language: 'xsd' | 'xpath';
 };
 
 /**
- * @param pattern Pattern to compile
- * @param options Additional options for the compiler
+ * Compiles the given pattern into a matching function. The returned function accepts a single
+ * string and returns true iff the pattern matches it.
  *
- * @return a matcher function, or null if the pattern uses unsupported features
+ * @public
+ *
+ * @param pattern - Pattern to compile
+ * @param options - Additional options for the compiler
+ *
+ * @returns a matcher function for the given pattern
  */
 export function compile(pattern: string, options: Options = { language: 'xsd' }): MatchFn {
 	const ast = generateParser(options)(pattern);
